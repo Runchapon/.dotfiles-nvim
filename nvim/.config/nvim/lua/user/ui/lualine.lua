087610c3904a8a9627aabb3383d362402deb6b3b
run <run10507@gmail.com>
2026-08-09 02:30:11 +0700

    add lualine

--- nvim/.config/nvim/lua/user/ui/lualine.lua
+++ nvim/.config/nvim/lua/user/ui/lualine.lua
@@ -68,12 +68,30 @@ function M.config()
 					"mode",
 					separator = { left = "" },
 					right_padding = 1,
-					icon_enabled = true,
-					icon = "",
 				},
 			},
 			lualine_b = {
-				{ "filename" },
+				{
+					"filename",
+					file_status = true, -- Displays file status (readonly status, modified status)
+					newfile_status = false, -- Display new file status (new file means no write after created)
+					path = 0, -- 0: Just the filename
+					-- 1: Relative path
+					-- 2: Absolute path
+					-- 3: Absolute path, with tilde as the home directory
+					-- 4: Filename and parent dir, with tilde as the home directory
+
+					shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+					-- for other components. (terrible name, any suggestions?)
+					-- It can also be a function that returns
+					-- the value of `shorting_target` dynamically.
+					symbols = {
+						modified = "[+]", -- Text to show when the file is modified.
+						readonly = "[-]", -- Text to show when the file is non-modifiable or readonly.
+						unnamed = "[No Name]", -- Text to show for unnamed buffers.
+						newfile = "[New]", -- Text to show for newly created file before first write
+					},
+				},
 				{ "branch" },
 			},
 			lualine_c = {
@@ -116,6 +134,64 @@ function M.config()
 					update_in_insert = false, -- Update diagnostics in insert mode.
 					always_visible = false, -- Show diagnostics even if there are none.
 				},
+				{
+					"searchcount",
+					fmt = function(input, tabnr)
+						if input ~= "" then
+							return "search count: " .. input
+						end
+					end,
+					timeout = 500,
+				},
+				{
+					"selectioncount",
+					fmt = function(input, tabnr)
+						if input ~= "" then
+							return "select count: " .. input
+						end
+					end,
+				},
+				{
+					"tabs",
+					tab_max_length = 40, -- Maximum width of each tab. The content will be shorten dynamically (example: apple/orange -> a/orange)
+					max_length = vim.o.columns / 3, -- Maximum width of tabs component.
+					-- Note:
+					-- It can also be a function that returns
+					-- the value of `max_length` dynamically.
+					mode = 0, -- 0: Shows tab_nr
+					-- 1: Shows tab_name
+					-- 2: Shows tab_nr + tab_name
+          icon = "tab",
+
+					path = 1, -- 0: just shows the filename
+					-- 1: shows the relative path and shorten $HOME to ~
+					-- 2: shows the full path
+					-- 3: shows the full path and shorten $HOME to ~
+
+					-- Automatically updates active tab color to match color of other components (will be overidden if buffers_color is set)
+					use_mode_colors = true,
+
+					tabs_color = {
+						-- Same values as the general color option can be used here.
+						active = "lualine_c_diff_added_insert", -- Color for active tab.
+						inactive = "lualine_c_inactive", -- Color for inactive tab.
+					},
+
+					show_modified_status = false, -- Shows a symbol next to the tab name if the file has been modified.
+					symbols = {
+						modified = "[+]", -- Text to show when the file is modified.
+					},
+
+					fmt = function(name, context)
+						-- Show + if buffer is modified in tab
+						local buflist = vim.fn.tabpagebuflist(context.tabnr)
+						local winnr = vim.fn.tabpagewinnr(context.tabnr)
+						local bufnr = buflist[winnr]
+						local mod = vim.fn.getbufvar(bufnr, "&mod")
+
+						return name .. (mod == 1 and " +" or "")
+					end,
+				},
 			},
 			lualine_x = {
 				{
@@ -124,28 +200,44 @@ function M.config()
 					color = { fg = "#ff9e64" },
 				},
 				{
-					-- Lsp server name .
-					function()
-						local msg = "No Active Lsp"
-						local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
-						local clients = vim.lsp.get_active_clients()
-						if next(clients) == nil then
-							return msg
-						end
-						for _, client in ipairs(clients) do
-							local filetypes = client.config.filetypes
-							if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-								return client.name
-							end
-							if client.name == "jdtls" then
-								return "jdtls"
-							end
-						end
-						return msg
-					end,
-					icon = " LSP:",
-					color = { fg = "#FFFFFF", gui = "bold" },
+					"lsp_status",
+					icon = " ", -- f013
+					symbols = {
+						-- Standard unicode symbols to cycle through for LSP progress:
+						spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" },
+						-- Standard unicode symbol for when LSP is done:
+						done = "✓",
+						-- Delimiter inserted between LSP names:
+						separator = " ",
+					},
+					-- List of LSP names to ignore (e.g., `null-ls`):
+					ignore_lsp = {},
+					-- Display the LSP name
+					show_name = true,
 				},
+				-- {
+				--   -- Lsp server name .
+				--   function()
+				--     local msg = "No Active Lsp"
+				--     local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
+				--     local clients = vim.lsp.get_active_clients()
+				--     if next(clients) == nil then
+				--       return msg
+				--     end
+				--     for _, client in ipairs(clients) do
+				--       local filetypes = client.config.filetypes
+				--       if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+				--         return client.name
+				--       end
+				--       if client.name == "jdtls" then
+				--         return "jdtls"
+				--       end
+				--     end
+				--     return msg
+				--   end,
+				--   icon = " LSP:",
+				--   color = { fg = "#FFFFFF", gui = "bold" },
+				-- },
 			},
 			lualine_y = {
 				{
@@ -170,15 +262,15 @@ function M.config()
 			},
 		},
 		inactive_sections = {
-			lualine_a = { "filename" },
+			lualine_a = {},
 			lualine_b = {},
 			lualine_c = {},
 			lualine_x = {},
 			lualine_y = {},
-			lualine_z = { "location" },
+			lualine_z = {},
 		},
 		tabline = {},
-		extensions = {},
+		extensions = { "mason" },
 	})
 end
 
